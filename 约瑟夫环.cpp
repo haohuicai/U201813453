bb8c58228b153bdaaf67182fa9fe02e0674b5f38
haohuicai <1002778980@qq.com>
2019-03-25 19:37:39 +0800

    实验报告

--- 约瑟夫环.cpp
+++ 约瑟夫环.cpp
@@ -6,35 +6,24 @@ struct Node{
 int data;
 int number;
 List next;
+int * Joseph(List p,int *a,int n);
 };
 List init()
 {
     List L =(List)malloc(sizeof(Node));
+    if(L==NULL)
+    {
+        printf("ÄÚ´æ²»×ã");
+    }
     L->next = NULL;
     return L;
 }
-int main(void)
+int *Joseph(List p,int *a,int n)
 {
-    int n;
-    scanf("%d",&n);
-    int i,j,t,k,m;
-    j = n;
-    int *a = (int*)malloc(n*sizeof(int));
-   List p,head,L,_L;
-   p = init();
-   head = p;
-   for(i=2;i<=n;i++)
-   {
-       L = init();
-       L->number = i;
-       scanf("%d",&L->data);
-       p->next = L;
-       p = L;
-   }
-   p->next = head;
-   head->number = 1;
-   printf("Input the first data\n");
-   scanf("%d",&head->data);
+    List head,_L;
+    head  = p;
+    int k,m;
+    int i;
    printf("Input the m\n");
    scanf("%d",&m);
    i = 0;
@@ -51,6 +40,38 @@ int main(void)
         free(_L);
    }
   a[n-1] = head->number;
+  return a;
+}
+int main(void)
+{
+    int n;
+    scanf("%d",&n);
+    if(n>20||n<0)
+    {
+        printf("Input error");
+        return 0;
+    }
+    int i,j,k,m;
+    j = n;
+    int *a = (int*)malloc(n*sizeof(int));
+   List p,head,L,_L;
+   p = init();
+   head = p;
+   for(i=2;i<=n;i++)
+   {
+       L = init();
+       L->number = i;
+       scanf("%d",&L->data);
+       if(L->data<0)
+        printf("error");
+       p->next = L;
+       p = L;
+   }
+    p->next = head;
+   head->number = 1;
+   printf("Input the first data\n");
+   scanf("%d",&head->data);
+  a = Joseph(head,a,n);
   for(i=0;i<n;i++)
   {
       printf("%d ",a[i]);
